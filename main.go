032a4f14066a1363170170bdb317e1dbe46af085
Kasra Karami <Kasra_K2K@yahoo.com>
2023-02-02 23:09:09 +0330

    uncomment codes

--- main.go
+++ main.go
@@ -1,29 +1,17 @@
 package main
 
-import "fmt"
-
-func evenOrOdd(number int) string {
-	return [2]string{"Even", "Odd"}[1&0]
-}
-
-func IfThenElse(condition bool, a, b interface{}) interface{} {
-	if condition {
-		return a
-	} else {
-		return b
-	}
-}
+import (
+	"fmt"
+	"goLang-Course/basics"
+)
 
 func main() {
-	//fmt.Println("goLang Tutorial with code and comment") // Example of print message
+	fmt.Println("goLang Tutorial with code and comment") // Example of print message
 	//
-	//basics.Variables() // In this line `basics` is name of package we wrote in variables file eq: package basics
-	//basics.Constants()
-	//basics.Print()
-	//basics.Functions()
-	//basics.Pointer()
-	//basics.For()
-
-	answer := evenOrOdd(11)
-	fmt.Println(answer)
+	basics.Variables() // In this line `basics` is name of package we wrote in variables file eq: package basics
+	basics.Constants()
+	basics.Print()
+	basics.Functions()
+	basics.Pointer()
+	basics.For()
 }
