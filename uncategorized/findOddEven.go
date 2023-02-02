af8ce365a0899f71f6458933a82e4bab8ebb0659
Kasra Karami <Kasra_K2K@yahoo.com>
2023-02-02 22:58:42 +0330

    change order

--- uncategorized/findOddEven.go
+++ uncategorized/findOddEven.go
@@ -18,14 +18,13 @@ func Two(number int) string {
 }
 
 func Three(number int) string {
-	return [2]string{"Even", "Odd"}[number&1]
-}
-
-func Four(number int) string {
 	return map[bool]string{true: "Even", false: "Odd"}[number%2 == 0]
 }
 
+func Four(number int) string {
+	return map[int]string{0: "Even", 1: "Odd"}[number%2]
+}
+
 func Five(number int) string {
-	parity := map[int]string{0: "Even", 1: "Odd"}
-	return parity[number%2]
+	return [2]string{"Even", "Odd"}[number&1]
 }
