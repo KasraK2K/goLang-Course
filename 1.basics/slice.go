c83836745e8bb42fc17ec6fc0cb16cfccf035262
Kasra Karami <Kasra_K2K@yahoo.com>
2023-02-11 19:25:47 +0330

    slice #23
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+Kasrak2K@users.noreply.github.com>

---
+++ 1.basics/slice.go
@@ -0,0 +1,53 @@
+package __basics
+
+import (
+	"fmt"
+)
+
+func Slice() {
+	fmt.Println("Learn Go Slice")
+
+	/* Slice */
+	/*
+		//--------------------------------------------------
+		s := make([]string, 3)
+		fmt.Println("emp:", s)
+
+		s[0] = "a"
+		s[1] = "b"
+		s[2] = "c"
+		fmt.Println("set:", s)
+		fmt.Println("get:", s[2])
+
+		fmt.Println("len:", len(s))
+
+		s = append(s, "d")
+		s = append(s, "e", "f")
+		fmt.Println("apd:", s)
+
+		c := make([]string, len(s))
+		copy(c, s)
+		fmt.Println("cpy:", c)
+
+		l := s[2:5]
+		fmt.Println("sl1:", l)
+
+		l = s[:5]
+		fmt.Println("sl2:", l)
+
+		l = s[2:]
+		fmt.Println("sl3:", l)
+
+		t := []string{"g", "h", "i"}
+		fmt.Println("dcl:", t)
+
+		twoD := [][]int{
+			{0},
+			{1, 2},
+			{3, 4, 5},
+		}
+		fmt.Println("2d: ", twoD)
+
+		//--------------------------------------------------
+	*/
+}
