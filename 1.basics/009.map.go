5cd9200a04be9cac6a0163f1a476174e274ff1d7
Kasra Karami <Kasra_K2K@yahoo.com>
2023-02-12 11:38:17 +0330

    maps #24
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: Kasra Karami <30779339+KasraK2K@users.noreply.github.com>

--- 1.basics/009.map.go
+++ 1.basics/009.map.go
@@ -10,7 +10,26 @@ func Map() {
 	/* Map */
 	/*
 		//--------------------------------------------------
+		m := make(map[string]int)
 
+		m["k1"] = 7
+		m["k2"] = 13
+
+		fmt.Println("map:", m)
+
+		v1 := m["k1"]
+		fmt.Println("v1: ", v1)
+
+		fmt.Println("len:", len(m))
+
+		delete(m, "k2")
+		fmt.Println("map:", m)
+
+		_, prs := m["k2"]
+		fmt.Println("prs:", prs)
+
+		n := map[string]int{"foo": 1, "bar": 2}
+		fmt.Println("map:", n)
 		//--------------------------------------------------
 	*/
 }
--- main.go
+++ main.go
@@ -3,26 +3,26 @@ package main
 import (
 	"fmt"
 	"goLang-Course/1.basics"
-	"goLang-Course/2.types"
 )
 
 func main() {
 	fmt.Println("goLang Tutorial with code and comment") // Example of print message
 
 	// Basics
-	__basics.Variables() // In this line `basics` is name of package we wrote in variables file eq: package basics
-	__basics.Constants()
-	__basics.Print()
-	__basics.Functions()
-	__basics.Pointer()
-	__basics.For()
-	__basics.Mutability()
-	__basics.If()
-	__basics.Switch()
-	__basics.Array()
-	__basics.Slice()
+	//__basics.Variables() // In this line `basics` is name of package we wrote in variables file eq: package basics
+	//__basics.Constants()
+	//__basics.Print()
+	//__basics.Functions()
+	//__basics.Pointer()
+	//__basics.For()
+	//__basics.Mutability()
+	//__basics.If()
+	//__basics.Switch()
+	//__basics.Array()
+	//__basics.Slice()
+	__basics.Map()
 
 	// Types
-	__types.BasicTypes()
-	__types.TypeConversion()
+	//__types.BasicTypes()
+	//__types.TypeConversion()
 }
