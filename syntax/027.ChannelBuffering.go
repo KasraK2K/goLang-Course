7572eaeaefc0fb52449957d733055c2e04d0e8d1
Kasra Karami <Kasra_K2K@yahoo.com>
2023-04-14 20:58:12 +0330

    Channel Buffering #57
    Explain channel buffering
    
    Co-Authored-By: kasrakaramikk <122204823+kasrakaramikk@users.noreply.github.com>

---
+++ syntax/027.ChannelBuffering.go
@@ -0,0 +1,22 @@
+package syntax
+
+import (
+	"fmt"
+)
+
+func ChannelBuffering() {
+	fmt.Println("Learn Go ChannelBuffering")
+
+	/* ChannelBuffering */
+	/*
+		//--------------------------------------------------
+		messages := make(chan string, 2) // Because of this channel buffered to have just 2 goroutines, if we pass more than 2 value we get fatal error: all goroutines are asleep - deadlock!
+
+		messages <- "buffered"
+		messages <- "channel"
+
+		fmt.Println(<-messages)
+		fmt.Println(<-messages)
+		//--------------------------------------------------
+	*/
+}
